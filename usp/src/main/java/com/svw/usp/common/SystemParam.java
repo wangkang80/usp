4427c491b3c249d020024ab5c0a88e08eb6ca474
wangkang <wangkangit@163.com>
2014-09-10 17:58:01 +0800

    初始化

---
+++ usp/src/main/java/com/svw/usp/common/SystemParam.java
@@ -0,0 +1,25 @@
+/**
+ * SystemParamEnum.java
+ * Created at 2014-03-07
+ * Created by wangkang
+ * Copyright (C) 2014 SHANGHAI VOLKSWAGEN, All rights reserved.
+ */
+package com.svw.usp.common;
+
+/**
+ * <p>
+ * ClassName: SYSTEM_PARAM_ENUM
+ * </p>
+ * <p>
+ * Description: 系统参数枚举
+ * </p>
+ * <p>
+ * Author: wangkang
+ * </p>
+ * <p>
+ * Date: 2014年3月7日
+ * </p>
+ */
+public enum SystemParam {
+
+}
